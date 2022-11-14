879ba13db708a872ef21f2f61df80fce3c2d48ab
Szalay Balázs <szalay.balazs.ms@gmail.com>
2022-11-14 13:34:56 +0100

    new command runner

--- index.js
+++ index.js
@@ -2,17 +2,10 @@
 
 const chalk = require('chalk');
 const commandExistsSync = require('command-exists').sync;
-const { exec } = require('child_process');
 const path = require('path');
+const spawn = require('cross-spawn');
 
 const arguments = process.argv.slice(2);
-
-const run = async (cmd) => {
-  const child = exec(cmd, { cwd: __dirname });
-  child.stderr.pipe(process.stderr);
-  child.stdout.pipe(process.stdout);
-  await new Promise((resolve) => child.on('close', resolve));
-};
 
 const tsInstalled = commandExistsSync('ts-node');
 
@@ -28,4 +21,4 @@ if (!tsInstalled) {
     chalk.cyan('npm i -g ts-node typescript'),
     chalk.dim('to install them.'),
   );
-} else run(`ts-node ${path.join(__dirname, 'src', 'index.ts')} ${arguments.join(' ')}`);
+} else spawn('ts-node', [path.join(__dirname, 'src', 'index.ts'), ...arguments], { stdio: 'inherit' });
